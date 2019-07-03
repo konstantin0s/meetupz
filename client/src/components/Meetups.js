0c5d42ec00b273cf57600546743acd613593a259
konstantin0s <constantintofan85@gmail.com>
2019-07-03 16:10:58 +0200

    Fetch data with axios and set state of response

--- client/src/components/Meetups.js
+++ client/src/components/Meetups.js
@@ -5,9 +5,23 @@ class Meetups extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            meetups: []
         };
     }
+
+    componentWillMount() {
+        this.getMeetups();
+    }
+
+    getMeetups() {
+        axios.get('http://localhost:3000/api/meetups')
+            .then(res => {
+                this.setState({ meetups: res.data }, () =>
+                    console.log(this.state))
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div>

f3d3a8ac3a996c77227ce7ea1c82878f0aec1f05
konstantin0s <constantintofan85@gmail.com>
2019-07-03 15:22:10 +0200

    Add Meetups, About as component pages

---
+++ client/src/components/Meetups.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Meetups extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h2>Meetups page.</h2>
+            </div>
+        );
+    }
+}
+
+export default Meetups;

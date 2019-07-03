5d4a8a46b17482478041304d3eb4b16d86fd46e9
konstantin0s <constantintofan85@gmail.com>
2019-07-03 18:44:32 +0200

    Create build app

---
+++ client/precache-manifest.031451a31ea9c646e1e01c65058f0a60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a2549d9861fe9eadd316284a0ac2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c66e1c66ccf24211988",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2ab5218d1fa7c4f17740",
+    "url": "/static/js/2.2a149e9e.chunk.js"
+  },
+  {
+    "revision": "6c66e1c66ccf24211988",
+    "url": "/static/js/main.c4bd3e4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

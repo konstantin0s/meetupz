0004dec43ad3648abe4932a402b6102492bd232c
konstantin0s <constantintofan85@gmail.com>
2019-07-03 17:59:24 +0200

    Delete a single meetup

--- client/src/components/MeetupDetails.js
+++ client/src/components/MeetupDetails.js
@@ -27,6 +27,17 @@ class MeetupDetails extends Component {
             .catch(err => console.log(err));
     }
 
+    onDelete = () => {
+        let meetupId = this.state.details.id;
+        axios.delete(`http://localhost:3000/api/meetups/${meetupId}`)
+            .then(res => {
+                this.props.history.push('/')
+
+            })
+            .catch(err => console.log(err));
+    }
+
+
     render() {
         return (
             <div>
@@ -39,7 +50,7 @@ class MeetupDetails extends Component {
                 </ul>
                 <Link className="btn"
                     to={`meetups/edit/${this.state.details.id}`}>Edit</Link>
-                <button className="btn red right">Delete</button>
+                <button onClick={this.onDelete.bind(this)} className="btn red right">Delete</button>
             </div>
         );
     }
